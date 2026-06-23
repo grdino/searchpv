3a31d81d87d0d05d29c27c4702fbf92d3a7663f0
grdino <67813644+grdino@users.noreply.github.com>
2026-06-23 12:22:39 -0600

    resend email fixes

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
     const body = await request.json();
 
     await resend.emails.send({
-      from: "SearchPV <onboarding@resend.dev>",
+      from: "SearchPV <requests@searchpv.com>",
       to: "gerry@ronmorgan.net",
       subject: `SearchPV Request - ${body.community}`,
       text: `
@@ -22,9 +22,6 @@ ${body.message}
     return NextResponse.json({ success: true });
   } catch (error) {
     console.error(error);
-    return NextResponse.json(
-      { success: false },
-      { status: 500 }
-    );
+    return NextResponse.json({ success: false }, { status: 500 });
   }
 }
