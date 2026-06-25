8f3d77a70ef0ca14b0f5a96d656b56d8bca1fd06
grdino <67813644+grdino@users.noreply.github.com>
2026-06-24 18:29:58 -0600

    fixed idx caching issue whith errors

--- src/lib/idx.ts
+++ src/lib/idx.ts
@@ -1,3 +1,4 @@
+/*
 export const IDX_SEARCH_URL =
   "https://idx.searchpv.com/idx/search/?Limit=150&ListingId=";
 
@@ -8,4 +9,19 @@ export function buildIdxUrl(listingIds: string) {
     .join(",");
 
   return `${IDX_SEARCH_URL}${limitedIds}`;
+}
+*/
+/* Fixed problem with caching errors */
+export const IDX_SEARCH_URL =
+  "https://idx.searchpv.com/idx/search/?Limit=150&ListingId=";
+
+export function buildIdxUrl(listingIds: string) {
+  const limitedIds = listingIds
+    .split(",")
+    .map((id) => id.trim())
+    .filter(Boolean)
+    .slice(0, 75)
+    .join(",");
+
+  return `${IDX_SEARCH_URL}${limitedIds}&cb=${Date.now()}`;
 }
