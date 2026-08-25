c6489f4da699e90841f08cfcb151e477f5f991dc
grdino <67813644+grdino@users.noreply.github.com>
2026-08-24 18:22:29 -0600

    upper case development link

--- src/app/components/atlas/AtlasMap.tsx
+++ src/app/components/atlas/AtlasMap.tsx
@@ -1437,7 +1437,7 @@ export default function AtlasMap() {
 
       marketUrl.searchParams.set(
         "development",
-        developmentName,
+        developmentName.toUpperCase(),
       );
 
       return marketUrl.toString();
